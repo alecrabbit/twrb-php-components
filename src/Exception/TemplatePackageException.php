4a2106cdca505e25809ca3b81195bab2d6e5d16e
Alec The Rabbit <alec.therabbit.git@gmail.com>
2018-10-31 14:04:58 +0200

    first

---
+++ src/Exception/TemplatePackageException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: alec
+ * Date: 14.10.18
+ * Time: 21:53
+ */
+
+namespace AlecRabbit\Exception;
+
+// Rename as needed & inherit from needed class
+class TemplatePackageException extends \Exception implements Exception
+{
+
+}
