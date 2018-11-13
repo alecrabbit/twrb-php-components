8976f49c0bbf363e15427740e91bb5eb1005972a
Alec The Rabbit <alec.therabbit.git@gmail.com>
2018-11-13 15:10:12 +0200

    mod

--- src/helpers/functions.php
+++ src/helpers/functions.php
@@ -1,6 +1,55 @@
 <?php
 /**
  * User: alec
- * Date: 06.11.18
- * Time: 15:00
+ * Date: 25.06.18
+ * Time: 12:07
  */
+
+if (!function_exists('c_avg')) {
+    /**
+     * @param float $first
+     * @param float $second
+     * @return float
+     */
+    function c_avg(float $first, float $second): float
+    {
+        return
+            ($first + $second) / 2;
+    }
+}
+
+if (!function_exists('c_array_aligned_sub')) {
+    /**
+     * @param array $first // minuend
+     * @param array $second // subtrahend
+     * @return array
+     */
+    function c_array_aligned_sub(array $first, array $second): array
+    {
+        $result = [];
+        foreach ($first as $key => $value) {
+            if (\array_key_exists($key, $second)) {
+                $result[$key] = $first[$key] - $second[$key];
+            }
+        }
+        return $result;
+    }
+}
+
+if (!function_exists('c_array_div')) {
+    /**
+     * @param array $arr
+     * @param float $divider
+     * @return array
+     */
+    function c_array_div(array $arr, float $divider): array
+    {
+        return
+            \array_map(
+                function ($v) use ($divider) {
+                    return
+                        $v / $divider;
+                },
+                $arr);
+    }
+}
---
+++ tests/Unit/Helpers/HelpersTest.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * User: alec
+ * Date: 12.11.18
+ * Time: 16:17
+ */
+
+namespace Tests\Unit\Helpers;
+
+
+use PHPUnit\Framework\TestCase;
+
+class HelpersTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider cAvgDataProvider
+     * @param $expected
+     * @param $first
+     * @param $second
+     */
+    public function FunctionCAvg($expected, $first, $second): void
+    {
+        $this->assertEquals($expected, c_avg($first, $second));
+    }
+
+    public function cAvgDataProvider(): array
+    {
+        return [
+            // [$expected, $first, $second],
+            [1, 1, 1],
+            [0.1, 0.1, 0.1],
+            [0.0001, 0.0001, 0.0001],
+            [0.00000001, 0.00000001, 0.00000001],
+            [2, 4, 0],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider cArrayDivDataProvider
+     * @param $expected
+     * @param $argument
+     * @param $divider
+     */
+    public function FunctionCArrayDiv($expected, $argument, $divider): void
+    {
+        $this->assertEquals($expected, c_array_div($argument, $divider));
+    }
+
+    public function cArrayDivDataProvider(): array
+    {
+        return [
+            // [$expected, $argument, $divider],
+            [[1], [1], 1],
+            [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 1],
+            [[1, 2, 3, 4, 5, 6], [2, 4, 6, 8, 10, 12], 2],
+            [[4, 8, 12, 16, 20, 24], [2, 4, 6, 8, 10, 12], 0.5],
+            [[2], [4], 2],
+        ];
+    }
+
+
+    /**
+     * @test
+     * @dataProvider cArrayAlignedSubDataProvider
+     * @param $expected
+     * @param $first
+     * @param $second
+     */
+    public function FunctionCArrayAlignedSub($expected, $first, $second): void
+    {
+        $this->assertEquals($expected, c_array_aligned_sub($first, $second));
+    }
+
+    public function cArrayAlignedSubDataProvider(): array
+    {
+        return [
+            // [$expected, $first, $second],
+            [[], [], []],
+            [
+                [
+                    30 => 3,
+                    40 => 3,
+                    50 => 3,
+                    60 => 3,
+                ],
+                [
+                    10 => 4,
+                    20 => 4,
+                    30 => 4,
+                    40 => 4,
+                    50 => 4,
+                    60 => 4,
+                ],
+                [
+                    30 => 1,
+                    40 => 1,
+                    50 => 1,
+                    60 => 1,
+                    70 => 1,
+                    80 => 1,
+                ]
+            ],
+        ];
+    }
+
+}
