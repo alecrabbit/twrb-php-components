25ef3a88cea0f8c2765141fe5c87c7c9822e7ed7
Alec The Rabbit <alec.therabbit.git@gmail.com>
2018-11-26 14:46:40 +0200

    mod

--- src/Money/Money.php
+++ src/Money/Money.php
@@ -8,7 +8,6 @@
 namespace AlecRabbit\Money;
 
 use AlecRabbit\Money\CalculatorFactory as Factory;
-use AlecRabbit\Money\Contracts\CalculatorInterface;
 use AlecRabbit\Money\Contracts\MoneyInterface;
 
 /**
@@ -20,9 +19,6 @@ class Money implements MoneyInterface, \JsonSerializable
 {
     use MoneyFactory,
         MoneyFunctions;
-
-    /** @var CalculatorInterface */
-    private $calculator;
 
     /** @var string */
     private $amount;
@@ -66,21 +62,6 @@ class Money implements MoneyInterface, \JsonSerializable
         $this->currency = $currency;
     }
 
-
-    /**
-     * Asserts that a Money has the same currency as this.
-     *
-     * @param Money $other
-     *
-     * @throws \InvalidArgumentException If $other has a different currency
-     */
-    private function assertSameCurrency(Money $other): void
-    {
-        if (!$this->isSameCurrency($other)) {
-            throw new \InvalidArgumentException('Currencies must be identical.');
-        }
-    }
-
     /**
      * Returns an integer less than, equal to, or greater than zero
      * if the value of this object is considered to be respectively
@@ -98,6 +79,20 @@ class Money implements MoneyInterface, \JsonSerializable
     }
 
     /**
+     * Asserts that a Money has the same currency as this.
+     *
+     * @param Money $other
+     *
+     * @throws \InvalidArgumentException If $other has a different currency
+     */
+    private function assertSameCurrency(Money $other): void
+    {
+        if (!$this->isSameCurrency($other)) {
+            throw new \InvalidArgumentException('Currencies must be identical.');
+        }
+    }
+
+    /**
      * Checks whether a Money has the same Currency as this.
      *
      * @param Money $other
@@ -110,56 +105,36 @@ class Money implements MoneyInterface, \JsonSerializable
     }
 
     /**
-     * Returns a new Money object that represents
-     * the sum of this and an other Money object.
+     * Returns the value represented by this object.
      *
-     * @param Money ...$addends
-     *
-     * @return Money
+     * @return string
      */
-    public function add(Money ...$addends): Money
+    public function getAmount(): string
     {
-        $amount = $this->amount;
-        $calculator = $this->calculator;
-
-        foreach ($addends as $addend) {
-            $this->assertSameCurrency($addend);
-
-            $amount = $calculator->add($amount, $addend->amount);
-        }
-        return new self($amount, $this->currency);
+        return $this->amount;
     }
 
     /**
-     * @param Money $first
-     * @param Money ...$collection
+     * Returns the currency of this object.
      *
-     * @return Money
+     * @return Currency
      */
-    public static function avg(Money $first, Money ...$collection): Money
+    public function getCurrency(): Currency
     {
-        return $first->add(...$collection)->divide(\func_num_args());
+        return $this->currency;
     }
 
     /**
-     * Returns a new Money object that represents
-     * the divided value by the given factor.
+     * {@inheritdoc}
      *
-     * @param float|int|string $divisor
-     *
-     * @return Money
+     * @return array
      */
-    public function divide($divisor): Money
+    public function jsonSerialize(): array
     {
-        $this->assertOperand($divisor);
-
-        if ($this->calculator->compare((string)$divisor, '0') === 0) {
-            throw new \InvalidArgumentException('Division by zero.');
-        }
-
-        $quotient = $this->calculator->divide($this->amount, $divisor);
-        return
-            $this->newInstance($quotient);
+        return [
+            'amount' => $this->amount,
+            'currency' => $this->currency,
+        ];
     }
 
     /**
@@ -190,295 +165,7 @@ class Money implements MoneyInterface, \JsonSerializable
      */
     private function newInstance($amount): Money
     {
-        return new self($amount, $this->currency);
+        return new Money($amount, $this->currency);
     }
 
-    /**
-     * Checks whether the value represented by this object equals to the other.
-     *
-     * @param Money $other
-     *
-     * @return bool
-     */
-    public function equals(Money $other): bool
-    {
-        return $this->isSameCurrency($other) && $this->amount === $other->amount;
-    }
-
-    /**
-     * @param Money $other
-     *
-     * @return bool
-     */
-    public function greaterThanOrEqual(Money $other): bool
-    {
-        return $this->compare($other) >= 0;
-    }
-
-    /**
-     * @param Money $other
-     *
-     * @return bool
-     */
-    public function lessThanOrEqual(Money $other): bool
-    {
-        return $this->compare($other) <= 0;
-    }
-
-    /**
-     * Returns the value represented by this object.
-     *
-     * @return string
-     */
-    public function getAmount(): string
-    {
-        return $this->amount;
-    }
-
-    /**
-     * Returns the currency of this object.
-     *
-     * @return Currency
-     */
-    public function getCurrency(): Currency
-    {
-        return $this->currency;
-    }
-
-    /**
-     * Returns a new Money object that represents
-     * the multiplied value by the given factor.
-     *
-     * @param float|int|string $multiplier
-     *
-     * @return Money
-     */
-    public function multiply($multiplier): Money
-    {
-        $this->assertOperand($multiplier);
-
-        $product = $this->calculator->multiply($this->amount, $multiplier);
-
-        return
-            $this->newInstance($product);
-    }
-
-    /**
-     * Returns a new Money object that represents
-     * the remainder after dividing the value by
-     * the given factor.
-     *
-     * @param Money $divisor
-     *
-     * @return Money
-     */
-    public function mod(Money $divisor): Money
-    {
-        $this->assertSameCurrency($divisor);
-
-        return new self($this->calculator->mod($this->amount, $divisor->amount), $this->currency);
-    }
-
-    /**
-     * Allocate the money among N targets.
-     *
-     * @param int $n
-     *
-     * @param int|null $precision
-     * @return Money[]
-     *
-     */
-    public function allocateTo(int $n, ?int $precision = null): array
-    {
-        if ($n <= 0) {
-            throw new \InvalidArgumentException('Number to allocateTo must be greater than zero.');
-        }
-
-        return $this->allocate(array_fill(0, $n, 1), $precision);
-    }
-
-    /**
-     * Allocate the money according to a list of ratios.
-     *
-     * @param array $ratios
-     *
-     * @param int|null $precision
-     * @return Money[]
-     */
-    public function allocate(array $ratios, ?int $precision = null): array
-    {
-        $precision = $precision ?? 2;
-        if (0 === $allocations = \count($ratios)) {
-            throw new \InvalidArgumentException('Cannot allocate to none, ratios cannot be an empty array.');
-        }
-
-        $remainder = $this->amount;
-        $results = [];
-        $total = array_sum($ratios);
-
-        if ($total <= 0) {
-            throw new \InvalidArgumentException('Sum of ratios must be greater than zero.');
-        }
-
-        foreach ($ratios as $ratio) {
-            if ($ratio < 0) {
-                throw new \InvalidArgumentException('Ratio must be zero or positive.');
-            }
-
-            $share = $this->calculator->share($this->amount, $ratio, $total, $precision);
-            $results[] = $this->newInstance($share);
-            $remainder = $this->calculator->subtract($remainder, $share);
-        }
-        switch ($this->calculator->compare($remainder, '0')) {
-            case -1:
-                for ($i = $allocations - 1; $i >= 0; $i--) {
-                    if (!$ratios[$i]) {
-                        continue;
-                    }
-                    $results[$i]->setAmount($this->calculator->add($results[$i]->amount, $remainder));
-                    break;
-                }
-                break;
-            case 1:
-                for ($i = 0; $i < $allocations; $i++) {
-                    if (!$ratios[$i]) {
-                        continue;
-                    }
-                    $results[$i]->setAmount($this->calculator->add($results[$i]->amount, $remainder));
-                    break;
-                }
-                break;
-            default:
-                break;
-        }
-        return $results;
-    }
-
-    /**
-     * @param Money $money
-     *
-     * @return string
-     */
-    public function ratioOf(Money $money): string
-    {
-        if ($money->isZero()) {
-            throw new \InvalidArgumentException('Cannot calculate a ratio of zero.');
-        }
-
-        return $this->calculator->divide($this->amount, $money->amount);
-    }
-
-    /**
-     * Checks if the value represented by this object is zero.
-     *
-     * @return bool
-     */
-    public function isZero(): bool
-    {
-        return $this->calculator->compare($this->amount, '0') === 0;
-    }
-
-    /**
-     * @return Money
-     */
-    public function absolute(): Money
-    {
-        return $this->newInstance($this->calculator->absolute($this->amount));
-    }
-
-    /**
-     * @return Money
-     */
-    public function negative(): Money
-    {
-        return $this->newInstance(0)->subtract($this);
-    }
-
-    /**
-     * Returns a new Money object that represents
-     * the difference of this and an other Money object.
-     *
-     * @param Money ...$subtrahends
-     *
-     * @return Money
-     */
-    public function subtract(Money ...$subtrahends): Money
-    {
-        $amount = $this->amount;
-        $calculator = $this->calculator;
-
-        foreach ($subtrahends as $subtrahend) {
-            $this->assertSameCurrency($subtrahend);
-
-            $amount = $calculator->subtract($amount, $subtrahend->amount);
-        }
-
-        return new self($amount, $this->currency);
-    }
-
-    /**
-     * Checks if the value represented by this object is not negative.
-     *
-     * @return bool
-     */
-    public function isNotNegative(): bool
-    {
-        return
-            !$this->isNegative();
-    }
-
-    /**
-     * Checks if the value represented by this object is negative.
-     *
-     * @return bool
-     */
-    public function isNegative(): bool
-    {
-        return $this->calculator->compare($this->amount, '0') === -1;
-    }
-
-    /**
-     * Checks if the value represented by this object is not zero.
-     *
-     * @return bool
-     */
-    public function isNotZero(): bool
-    {
-        return
-            !$this->isZero();
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @return array
-     */
-    public function jsonSerialize(): array
-    {
-        return [
-            'amount' => $this->amount,
-            'currency' => $this->currency,
-        ];
-    }
-
-    /**
-     * Checks if the value represented by this object is not positive.
-     *
-     * @return bool
-     */
-    public function isNotPositive(): bool
-    {
-        return
-            !$this->isPositive();
-    }
-
-    /**
-     * Checks if the value represented by this object is positive.
-     *
-     * @return bool
-     */
-    public function isPositive(): bool
-    {
-        return $this->calculator->compare($this->amount, '0') === 1;
-    }
 }
--- src/Money/MoneyFunctions.php
+++ src/Money/MoneyFunctions.php
@@ -7,9 +7,14 @@
 
 namespace AlecRabbit\Money;
 
+use AlecRabbit\Money\Contracts\CalculatorInterface;
 
 trait MoneyFunctions
 {
+    /** @var CalculatorInterface */
+    private $calculator;
+
+
     /**
      * @param Money $first
      * @param Money ...$collection
@@ -83,6 +88,315 @@ trait MoneyFunctions
         return $first->add(...$collection);
     }
 
+    /**
+     * Returns a new Money object that represents
+     * the sum of this and an other Money object.
+     *
+     * @param Money ...$addends
+     *
+     * @return Money
+     */
+    public function add(Money ...$addends): Money
+    {
+        $amount = $this->amount;
+        $calculator = $this->calculator;
 
+        foreach ($addends as $addend) {
+            $this->assertSameCurrency($addend);
+
+            $amount = $calculator->add($amount, $addend->amount);
+        }
+        return new Money($amount, $this->currency);
+    }
+
+    /**
+     * @param Money $first
+     * @param Money ...$collection
+     *
+     * @return Money
+     */
+    public static function avg(Money $first, Money ...$collection): Money
+    {
+        return $first->add(...$collection)->divide(\func_num_args());
+    }
+
+    /**
+     * Returns a new Money object that represents
+     * the divided value by the given factor.
+     *
+     * @param float|int|string $divisor
+     *
+     * @return Money
+     */
+    public function divide($divisor): Money
+    {
+        $this->assertOperand($divisor);
+
+        if ($this->calculator->compare((string)$divisor, '0') === 0) {
+            throw new \InvalidArgumentException('Division by zero.');
+        }
+
+        $quotient = $this->calculator->divide($this->amount, $divisor);
+        return
+            $this->newInstance($quotient);
+    }
+
+    /**
+     * Checks whether the value represented by this object equals to the other.
+     *
+     * @param Money $other
+     *
+     * @return bool
+     */
+    public function equals(Money $other): bool
+    {
+        return $this->isSameCurrency($other) && $this->amount === $other->amount;
+    }
+
+    /**
+     * @param Money $other
+     *
+     * @return bool
+     */
+    public function greaterThanOrEqual(Money $other): bool
+    {
+        return $this->compare($other) >= 0;
+    }
+
+    /**
+     * @param Money $other
+     *
+     * @return bool
+     */
+    public function lessThanOrEqual(Money $other): bool
+    {
+        return $this->compare($other) <= 0;
+    }
+
+    /**
+     * Returns a new Money object that represents
+     * the multiplied value by the given factor.
+     *
+     * @param float|int|string $multiplier
+     *
+     * @return Money
+     */
+    public function multiply($multiplier): Money
+    {
+        $this->assertOperand($multiplier);
+
+        $product = $this->calculator->multiply($this->amount, $multiplier);
+
+        return
+            $this->newInstance($product);
+    }
+
+    /**
+     * Returns a new Money object that represents
+     * the remainder after dividing the value by
+     * the given factor.
+     *
+     * @param Money $divisor
+     *
+     * @return Money
+     */
+    public function mod(Money $divisor): Money
+    {
+        $this->assertSameCurrency($divisor);
+
+        return new Money($this->calculator->mod($this->amount, $divisor->amount), $this->currency);
+    }
+
+    /**
+     * Allocate the money among N targets.
+     *
+     * @param int $n
+     *
+     * @param int|null $precision
+     * @return Money[]
+     *
+     */
+    public function allocateTo(int $n, ?int $precision = null): array
+    {
+        if ($n <= 0) {
+            throw new \InvalidArgumentException('Number to allocateTo must be greater than zero.');
+        }
+
+        return $this->allocate(array_fill(0, $n, 1), $precision);
+    }
+
+    /**
+     * Allocate the money according to a list of ratios.
+     *
+     * @param array $ratios
+     *
+     * @param int|null $precision
+     * @return Money[]
+     */
+    public function allocate(array $ratios, ?int $precision = null): array
+    {
+        $precision = $precision ?? 2;
+        if (0 === $allocations = \count($ratios)) {
+            throw new \InvalidArgumentException('Cannot allocate to none, ratios cannot be an empty array.');
+        }
+
+        $remainder = $this->amount;
+        $results = [];
+        $total = array_sum($ratios);
+
+        if ($total <= 0) {
+            throw new \InvalidArgumentException('Sum of ratios must be greater than zero.');
+        }
+
+        foreach ($ratios as $ratio) {
+            if ($ratio < 0) {
+                throw new \InvalidArgumentException('Ratio must be zero or positive.');
+            }
+
+            $share = $this->calculator->share($this->amount, $ratio, $total, $precision);
+            $results[] = $this->newInstance($share);
+            $remainder = $this->calculator->subtract($remainder, $share);
+        }
+        switch ($this->calculator->compare($remainder, '0')) {
+            case -1:
+                for ($i = $allocations - 1; $i >= 0; $i--) {
+                    if (!$ratios[$i]) {
+                        continue;
+                    }
+                    $results[$i]->setAmount($this->calculator->add($results[$i]->amount, $remainder));
+                    break;
+                }
+                break;
+            case 1:
+                for ($i = 0; $i < $allocations; $i++) {
+                    if (!$ratios[$i]) {
+                        continue;
+                    }
+                    $results[$i]->setAmount($this->calculator->add($results[$i]->amount, $remainder));
+                    break;
+                }
+                break;
+            default:
+                break;
+        }
+        return $results;
+    }
+
+    /**
+     * @param Money $money
+     *
+     * @return string
+     */
+    public function ratioOf(Money $money): string
+    {
+        if ($money->isZero()) {
+            throw new \InvalidArgumentException('Cannot calculate a ratio of zero.');
+        }
+
+        return $this->calculator->divide($this->amount, $money->amount);
+    }
+
+    /**
+     * Checks if the value represented by this object is zero.
+     *
+     * @return bool
+     */
+    public function isZero(): bool
+    {
+        return $this->calculator->compare($this->amount, '0') === 0;
+    }
+
+    /**
+     * @return Money
+     */
+    public function absolute(): Money
+    {
+        return $this->newInstance($this->calculator->absolute($this->amount));
+    }
+
+    /**
+     * @return Money
+     */
+    public function negative(): Money
+    {
+        return $this->newInstance(0)->subtract($this);
+    }
+
+    /**
+     * Returns a new Money object that represents
+     * the difference of this and an other Money object.
+     *
+     * @param Money ...$subtrahends
+     *
+     * @return Money
+     */
+    public function subtract(Money ...$subtrahends): Money
+    {
+        $amount = $this->amount;
+        $calculator = $this->calculator;
+
+        foreach ($subtrahends as $subtrahend) {
+            $this->assertSameCurrency($subtrahend);
+
+            $amount = $calculator->subtract($amount, $subtrahend->amount);
+        }
+
+        return new Money($amount, $this->currency);
+    }
+
+    /**
+     * Checks if the value represented by this object is not negative.
+     *
+     * @return bool
+     */
+    public function isNotNegative(): bool
+    {
+        return
+            !$this->isNegative();
+    }
+
+    /**
+     * Checks if the value represented by this object is negative.
+     *
+     * @return bool
+     */
+    public function isNegative(): bool
+    {
+        return $this->calculator->compare($this->amount, '0') === -1;
+    }
+
+    /**
+     * Checks if the value represented by this object is not zero.
+     *
+     * @return bool
+     */
+    public function isNotZero(): bool
+    {
+        return
+            !$this->isZero();
+    }
+
+    /**
+     * Checks if the value represented by this object is not positive.
+     *
+     * @return bool
+     */
+    public function isNotPositive(): bool
+    {
+        return
+            !$this->isPositive();
+    }
+
+    /**
+     * Checks if the value represented by this object is positive.
+     *
+     * @return bool
+     */
+    public function isPositive(): bool
+    {
+        return $this->calculator->compare($this->amount, '0') === 1;
+    }
+
+    abstract public function compare(Money $other): int;
 
 }
